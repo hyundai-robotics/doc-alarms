db218bc9f340302ef7672e5548be4b05331ca5ac
최원혁 <asoe72@gmail.com>
2026-03-07 19:12:10 +0900

    check_json_format 파일들 추가

---
+++ hi6/check.bat
@@ -0,0 +1,29 @@
+@echo off
+
+echo ko =======================================
+echo.
+echo help_err_ko.json:
+node .\check_json_format.js ./help_err_ko.json
+
+echo.
+echo help_warn_ko.json:
+node .\check_json_format.js ./help_warn_ko.json
+
+echo.
+echo help_noti_ko.json:
+node .\check_json_format.js ./help_noti_ko.json
+
+echo en =======================================
+echo.
+echo help_err_en.json:
+node .\check_json_format.js ./help_err_en.json
+
+echo.
+echo help_warn_en.json:
+node .\check_json_format.js ./help_warn_en.json
+
+echo.
+echo help_noti_en.json:
+node .\check_json_format.js ./help_noti_en.json
+
+pause
